888f1395314742230d35ce7371a2c88b25694be8
Raffa064 <raffaddtg@gmail.com>
2024-09-07 01:28:20 -0300

    Implemented proxy router
    The server has two new routers, the static one is at the root, and the
    "proxy/" can be used as an download proxy.
    
    proxy router has been retricted by user name and password that must be
    passed inside it's request body in json format.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,52 @@
+const path = require("path")
+const https = require("https")
 const express = require("express")
 const app = express()
 
-app.get("/", (req, res) => {
-  res.send("Ok")
+const staticRouter = express.Router("/")
+staticRouter.use(express.static(path.join(__dirname, "../static")))
+
+const proxyRouter = express.Router("/proxy")
+proxyRouter.use(express.json())
+proxyRouter.use((req, res, next) => {
+  const { user, password } = req.body;
+
+  if (req.route == "/proxy" && !checkCredentials(user, password)) {
+    res.status(401).send("Invalid credentials")
+    console.log(Date.now(), " Rejected request for ", req.body)
+    return
+  }
+
+  next()
 })
+
+proxyRouter.post("/proxy", (req, res) => {
+  const url = req.query.url
+  downloadStream(url, res)
+  console.log("Initilized piped download")
+})
+
+
+app.use(staticRouter)
+app.use(proxyRouter)
 
 const PORT = process.env.PORT || 8080
 
 app.listen(PORT, () => {
   console.log(`Server started on port: ${PORT}`)
 })
+
+function downloadStream(url, outputPipe) {
+  https.get(url, (res) => {
+    res.pipe(outputPipe)
+    res.on("finish", () => {
+      outputPipe.close()
+      console.log("Finished download")
+    })
+  })
+}
+
+function checkCredentials(user, password) {
+  const { CR_USER_NAME, CR_PASSWORD } = process.env
+  return user == CR_USER_NAME && password == CR_PASSWORD
+}
